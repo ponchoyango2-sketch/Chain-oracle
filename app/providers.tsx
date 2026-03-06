87f4100ac912bd92455a2176cd4cc8919391e349
ponchoyango2-sketch <ponchoyango2@gmail.com>
2026-03-06 15:12:57 -0800

    Update Providers to disable SSR and adjust imports

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,13 +4,12 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { ReactNode } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
 
 import {
-  getDefaultConfig,
   RainbowKitProvider,
+  getDefaultConfig,
 } from "@rainbow-me/rainbowkit";
-
-import { WagmiProvider } from "wagmi";
 
 import { base } from "wagmi/chains";
 
@@ -18,7 +17,7 @@ const config = getDefaultConfig({
   appName: "Chain Oracle",
   projectId: "c72acb0517b84dde28476773e31de1da",
   chains: [base],
-  ssr: true,
+  ssr: false,
 });
 
 const queryClient = new QueryClient();
@@ -27,7 +26,7 @@ export default function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <WagmiProvider config={config}>
-        <RainbowKitProvider coolMode>
+        <RainbowKitProvider>
           {children}
         </RainbowKitProvider>
       </WagmiProvider>
