48896e9746ecd018666cc2108913ca1efd7ce215
ponchoyango2-sketch <ponchoyango2@gmail.com>
2026-03-08 10:34:08 -0700

    Refactor Providers component for Wagmi and QueryClient

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,21 +2,21 @@
 
 import "@rainbow-me/rainbowkit/styles.css";
 import { ReactNode } from "react";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { WagmiProvider } from "wagmi";
-
+import { WagmiProvider, http } from "wagmi";
+import { base } from "wagmi/chains";
 import {
   RainbowKitProvider,
   getDefaultConfig,
 } from "@rainbow-me/rainbowkit";
 
-import { base } from "wagmi/chains";
-
 const config = getDefaultConfig({
   appName: "Chain Oracle",
   projectId: "c72acb0517b84dde28476773e31de1da",
   chains: [base],
+  transports: {
+    [base.id]: http(),
+  },
   ssr: true,
 });
 
@@ -24,12 +24,12 @@ const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <WagmiProvider config={config}>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           {children}
         </RainbowKitProvider>
-      </WagmiProvider>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
